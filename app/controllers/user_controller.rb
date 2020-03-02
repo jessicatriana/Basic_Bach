0122983eefd2c7f71d2d60eb47401ef6b5d4737c
github username <github email address>
2020-03-02 16:46:21 -0600

    Made some crud changes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,17 @@
 class UserController < ApplicationController
   def new
+    @user = User.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 end
