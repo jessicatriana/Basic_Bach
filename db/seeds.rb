87d62e56721ea1e4448e0e08dadb1e157fe89fa3
jessicatriana <jessica.triana.tx@gmail.com>
2020-03-02 17:22:19 -0600

    Update Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,6 +7,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+d = Date.parse('3rd Feb 2001')
 
 avi = User.create(name: "Avi", role: "Host")
 tom = User.create(name: "Tom", role: "Host")
