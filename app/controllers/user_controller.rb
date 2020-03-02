65e85038db2bce00d02b37ce8c9d36cebb195ca1
github username <github email address>
2020-03-02 17:27:06 -0600

    Need to recommit to seed

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,9 @@
 class UserController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+  
   def new
     @user = User.new
   end
@@ -7,11 +12,24 @@ class UserController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def index
-    @users = User.all
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if user.valid?
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :role)
+  end
+
 end
