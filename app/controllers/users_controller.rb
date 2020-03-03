35fbabc99afa665959f6546ee54523eb456ef036
jessicatriana <jessica.triana.tx@gmail.com>
2020-03-02 21:17:48 -0600

    Completed Activities CRUD

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user = User.update(user_params)
+    if user.valid? 
+      redirect_to user_path(@user)
+    else 
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :role)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :activities
-  resources :events
-  resources :users
+  resources :activities, :events, :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
