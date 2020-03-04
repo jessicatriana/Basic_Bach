22efb886c13edfbf9ea21eac49d085652fcfdfbb
jessicatriana <jessica.triana.tx@gmail.com>
2020-03-04 16:21:28 -0600

    Final Touches

--- app/models/event_activity.rb
+++ app/models/event_activity.rb
@@ -1,9 +1,4 @@
 class EventActivity < ApplicationRecord
-<<<<<<< HEAD
-    belongs_to :event 
-    belongs_to :activity
-=======
     belongs_to :event, dependent: :destroy
-    belongs_to :user, dependent: :destroy
->>>>>>> 26919e5d1e053f8eb09fb93b333ba5f5066724bb
+    belongs_to :activity, dependent: :destroy
 end
