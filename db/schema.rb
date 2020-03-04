ab6c33458a24907401f93b982d9d3904494b7df7
github username <github email address>
2020-03-04 13:01:09 -0600

    Cleaned and added new seeds. Add table column to Activities for images. Swapped teal home page logo for white.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_212546) do
+ActiveRecord::Schema.define(version: 2020_03_04_175048) do
 
   create_table "activities", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "event_activities", force: :cascade do |t|
