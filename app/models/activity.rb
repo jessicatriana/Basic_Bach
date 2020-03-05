d58d6ad9edc136f1941e80acc25a845f597b3391
github username <github email address>
2020-03-05 10:02:09 -0600

    Added audio

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,11 @@
 class Activity < ApplicationRecord
     has_many :event_activities
     has_many :events, through: :event_activities
+
+    def you_party_brah?
+        if self.title == "Antonio's Super Secret DJ Parties"
+            return true
+        end
+    end
+
 end
